4e01838a18fbd691f38d59d2e86150ba84223b9e
Shivam Rajput <rajputshivam8550855@gmail.com>
2025-10-18 04:51:32 +0530

    code

--- Array/ElementOfArray.java
+++ Array/ElementOfArray.java
@@ -15,5 +15,13 @@ public class ElementOfArray{
         for(int i=0; i<arr.length; i++){
             System.out.print(arr[i]+" ");
         }
+
+        //// for negative element print
+        System.out.println();
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] < 0){
+                System.out.print(arr[i]+" ");
+            }
+        }
     }
 }
