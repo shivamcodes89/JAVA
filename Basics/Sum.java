9ab4acc1fcc83fcc0c0c75cfb11e14cc425a98ac
Shivam Rajput <rajputshivam8550855@gmail.com>
2025-10-17 17:36:24 +0530

    code

---
+++ Basics/Sum.java
@@ -0,0 +1,8 @@
+public class Sum {
+    public static void main(String[] args) {
+        int a = 5;
+        float b = 6.89f;
+        double sum = a + b;
+        System.out.printf("%.2f", sum); 
+    }
+}
