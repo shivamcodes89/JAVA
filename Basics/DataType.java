2725f6b0f4a2f5438e2562c44ebf6b77283125fb
Shivam Rajput <rajputshivam8550855@gmail.com>
2025-10-17 17:28:50 +0530

    code

---
+++ Basics/DataType.java
@@ -0,0 +1,14 @@
+public class DataType{
+    public static void main(String[] args) {
+        int i = 3;
+        String s = "shivam";
+        float f = 3.1343f;
+        double d = 3.367;
+
+        System.out.println(i);
+        System.out.println(s);
+        System.out.println(f);
+        System.out.println(d);
+
+    }
+}
