aee93376d4d5ba985d13a3a98a845cc2f89a8da9
Shivam Rajput <rajputshivam8550855@gmail.com>
2025-10-17 21:58:26 +0530

    code

---
+++ Basics/SimpleInterest.java
@@ -0,0 +1,9 @@
+public class SimpleInterest {
+    public static void main(String[] args) {
+        int p = 2928;
+        float t = 2.5f;
+        float r = 5.5f;
+        float SI = (p * t * r) / 100;
+        System.out.println(SI);
+    }
+}
