6a6d0bf8813e076732ee650f0b37917e6d650a71
Shivam Rajput <rajputshivam8550855@gmail.com>
2025-10-17 19:55:01 +0530

    code

--- Basics/Temperature.java
+++ Basics/Temperature.java
@@ -3,9 +3,15 @@ import java.util.Scanner;
 public class Temperature {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter temperature in Fahrenheit: ");
+        // System.out.println("enter temperature in Fahrenheit: ");
+        // float temp1 = sc.nextFloat();
+        // float temp2 = (temp1 - 32) * (5f/9);
+        // System.out.println(temp2);
+
+
+        System.out.println("enter temperature in celsius: ");
         float temp1 = sc.nextFloat();
-        float temp2 = (temp1 - 32) * (5f/9);
+        float temp2 = ((9/5f) * temp1) + 32;
         System.out.println(temp2);
     }
 }
