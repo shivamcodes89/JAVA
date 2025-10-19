40d4f601b14dbdc9f0b7bdbd0fb5f48e69a15cd0
Shivam Rajput <rajputshivam8550855@gmail.com>
2025-10-19 13:30:12 +0530

    code

--- OOPs/Car.java
+++ OOPs/Car.java
@@ -1,9 +1,47 @@
 public class Car {
-    String color;
-    String brand;
-    String model;
-    int year;
-    int speed;
+    private String color;
+    private String brand;
+    private String model;
+    private int year;
+    private int speed;
+
+    public Car(String brand, String model, int year){
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+
+    }
+
+    public String getColor(){
+        return color;
+    }
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public String getBrand(){
+        return brand;
+    }
+
+    public String getModel(){
+        return model;
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    public int getSpeed(){
+        return speed;
+    }
+    public void setSpeed(int speed){
+        this.speed = speed;
+    }
+    
+
+
+
+
 
     public void accelerate(int increment){
         speed += increment;
--- OOPs/Car_Main.java
+++ OOPs/Car_Main.java
@@ -1,15 +1,12 @@
 public class Car_Main {
     public static void main(String[] args) {
-        Car car = new Car();
+        Car car = new Car("LAND ROVER", "DEFENDER P400", 2025);
 
-        car.color = "Blue";
-        car.speed = 40;
-        car.brand = "LAND ROVER";
-        car.model = "DEFENDER P400";
-        car.year = 2025;
+        car.setColor("blue");
+        car.setSpeed(40);
 
         car.accelerate(1);
 
-        System.out.println(car.speed);
+        System.out.println(car.getSpeed());
     }
 }
