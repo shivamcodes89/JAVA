02198572a9bed4f406f22a3532b9fb86515f9d61
Shivam Rajput <rajputshivam8550855@gmail.com>
2025-10-19 17:33:16 +0530

    code

--- OOPs/Bank_Main.java
+++ OOPs/Bank_Main.java
@@ -11,3 +11,62 @@ public class Bank_Main{
         System.out.println("your balance is: " + bank.getBalance());
     }
 }
+
+ class Bank{
+    private int bankaccount;
+    private String accountHolder;
+    private double balance;
+    private String IFSC;
+    private String branch;
+
+
+    public Bank(int bankaccount, String accountholder, double StartingBalance, String IFSC, String brance){
+        this.bankaccount = bankaccount;
+        this.accountHolder = accountholder;
+        this.balance = StartingBalance;
+        this.IFSC = IFSC;
+        this.branch = brance;
+
+    }
+    
+    
+    public int getBankAccount(){
+        return bankaccount;
+    }
+    public String getAccountHolder(){
+        return accountHolder;
+    }
+    public double getBalance(){
+        return balance;
+    }
+    public String getIfsc(){
+        return IFSC;
+    }
+    public String getBranch(){
+        return branch;
+    }
+
+
+    public void credit(double amount){
+        if(amount > 0){
+            balance = amount + balance;
+            System.out.println("credited: " + balance);
+        }
+        else{
+            System.out.println("invalid amount");
+        }
+
+    }
+
+
+    public void debit(double amount){
+        if(amount > 0 && balance > amount){
+            balance = balance - amount;
+            System.out.println("debited: " + amount);
+        }
+        else{
+            System.out.println("invalid");
+        }
+    }
+}
+
